99d14743ab3a6b4f8eafcdf0678e62949e0564aa
rpeterson87 <oshorhino5053@gmail.com>
2022-08-07 13:14:49 -0400

    Sudo Code Complete

---
+++ Go-Go-Gecko.js
@@ -0,0 +1,26 @@
+//* Add game variables - Name of character - Obstacles etc.
+
+//* The player will start the game by either hitting the space bar or the up arrow key
+
+//* The player will need to jump over obstacle's while they move across the screen.
+
+//? Need to add a timer and win loose message to the screen so the player will know the status of the game.
+
+//? Need to setup how to win/lose:
+
+//! The player will win by not hitting any obstacles for 15sec
+
+//! The player will loose if they hit an obstacle.
+
+
+
+//* Variables
+
+
+
+//? Functions
+
+
+
+
+//! EventListeners 
